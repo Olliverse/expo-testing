5aafecb1c9e3276b7cbba3853771450fd90cbd35
olli <oliverrausch2@web.de>
2024-04-01 19:01:45 +0200

    add colors to theme provider

--- contexts/ThemeContext.js
+++ contexts/ThemeContext.js
@@ -1,12 +1,30 @@
 import React, {createContext, useContext, useMemo, useState} from 'react';
 import PropTypes from "prop-types";
 
+
+const colors = {
+    light: {
+        background: '#F5F5F5',
+        primary: '#007AFF',
+        secondary: '#9C27B0',
+        text: '#000000',
+        font: '#000000',
+    },
+    dark: {
+        background: '#212121',
+        primary: '#FF4081',
+        secondary: '#4CAF50',
+        text: '#FFFFFF',
+        font: '#FFFFFF',
+    },
+};
+
 const DEFAULT_VALUE = 'light'
 const ThemeContext = createContext(DEFAULT_VALUE);
 
 export const ThemeProvider = ({children}) => {
     const [theme, setTheme] = useState(DEFAULT_VALUE);
-    const contextValue = useMemo(() => ({theme, setTheme}), [theme]);
+    const contextValue = useMemo(() => ({theme, setTheme, colors: colors[theme]}), [theme]);
 
     return (
         <ThemeContext.Provider value={contextValue}>
