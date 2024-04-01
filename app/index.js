cfe306901fd860baa012278022e37b76570ccdf9
Oliver Rausch <oliver.rausch@rwu.de>
2024-04-02 01:36:14 +0200

    ????

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 import {StatusBar} from "expo-status-bar";
-import {Dimensions, Pressable, StyleSheet, Text, View} from "react-native";
+import {Dimensions, Pressable, StyleSheet, Switch, Text, View} from "react-native";
 import {Link} from "expo-router";
 
 import Button from '../components/commons/Button';
@@ -9,11 +9,23 @@ import HorizontalSwipeView from "../components/HorizontalSwipeView";
 import RandomAppInfo from "../components/native_components/RandomAppInfo";
 import Frieren from "../components/Frieren";
 import {Gesture, GestureHandlerRootView} from "react-native-gesture-handler";
-import ThemeSwitch from "../components/commons/ThemeSwitch";
 import {useThemeState} from "../contexts/ThemeContext";
+import {useEffect, useState} from "react";
+import {dark, light} from "../styles/colors";
 
 export default function AppContainer() {
     const {theme, setTheme} = useThemeState();
+    const [darkMode, setDarkMode] = useState(theme.background === "#212121");
+
+    useEffect(() => {
+        if (darkMode) {
+            setTheme("dark")
+        }
+        if (!darkMode) {
+            setTheme("light")
+        }
+    }, [darkMode]);
+
     const horizontalSwipeGesture = Gesture.Pan()
 
     return (
@@ -23,7 +35,18 @@ export default function AppContainer() {
             <GestureHandlerRootView style={[styles.gestureContainer, {backgroundColor: theme.background}]}>
                 <HorizontalSwipeView horizontalSwipeGesture={horizontalSwipeGesture}>
                     <View style={[styles.innerContainer, {backgroundColor: theme.background}]}>
-                        <ThemeSwitch setTheme ={setTheme}/>
+                        <View style={[styles.container, {backgroundColor: theme.background}]}>
+                            <Text style={[styles.label, {color: theme.text}]}>
+                                Dark Mode
+                            </Text>
+                            <Switch
+                                trackColor={{false: light.primary2, true: dark.primary2}}
+                                thumbColor={darkMode ? dark.background : light.background}
+                                // ios_backgroundColor="#3e3e3e"
+                                onValueChange={setDarkMode}
+                                value={darkMode}
+                            />
+                        </View>
                     </View>
 
                     <View style={[styles.innerContainer, {backgroundColor: theme.background}]}>
@@ -56,7 +79,7 @@ export default function AppContainer() {
         </View>
     );
 }
-
+// StyleSheet.
 const styles = StyleSheet.create({
     mainContainer: {
         display: "flex",
@@ -75,5 +98,19 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         width: Dimensions.get('window').width,
         height: '100%'
+    },
+
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        borderRadius: 20,
+        elevation: 3,
+    },
+    label: {
+        fontSize: 16,
+        fontWeight: 'bold',
     },
 });
