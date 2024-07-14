bdd655c3acf303e14ded59bcd965b52ca67102fc
olli <oliverrausch2@web.de>
2024-07-14 13:14:59 +0200

    update

--- components/commons/LinkButton.js
+++ components/commons/LinkButton.js
@@ -2,27 +2,26 @@ import React from 'react';
 import {Pressable, StyleSheet, Text} from 'react-native';
 import PropTypes from 'prop-types';
 import {useThemeState} from '../../contexts/ThemeContext';
-import {router} from "expo-router";
+import {Link} from "expo-router";
 
 export default function LinkButton({label, path, style}) {
     const {theme} = useThemeState();
 
-    const handlePress = () => {
-        router.push(path);
-    };
-
     return (
-        <Pressable onPress={handlePress} style={style}>
-            <Text style={[styles.link, {color: theme.primary1}]}>
-                {label}
-            </Text>
-        </Pressable>
+        <Link href={path} asChild >
+            <Pressable style={style}>
+                <Text style={[styles.link, {color: theme.primary1}]}>
+                    {label}
+                </Text>
+            </Pressable>
+        </Link>
     );
 }
 
 LinkButton.propTypes = {
     label: PropTypes.string,
     path: PropTypes.string,
+    style: PropTypes.object
 };
 
 const styles = StyleSheet.create({
