413096826f1ac5cbd4fbdc3d7416e6390b7b6605
olli <oliverrausch2@web.de>
2024-07-15 11:56:23 +0200

    remove keys

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,15 +1,15 @@
-import {initializeApp} from "firebase/app"
+import { initializeApp } from "firebase/app"
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
-import {getReactNativePersistence, initializeAuth} from "firebase/auth"
+import {initializeAuth, getReactNativePersistence } from "firebase/auth"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDh6kOP1_kcslbos6TPNwIV9NNCQkjx4r4",
-    authDomain: "expo-testing-692aa.firebaseapp.com",
-    projectId: "expo-testing-692aa",
-    storageBucket: "expo-testing-692aa.appspot.com",
-    messagingSenderId: "595842757136",
-    appId: "1:595842757136:web:1dc20d3058f1fbe0af5095",
-    measurementId: "G-JQ1LS0WHT5",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
