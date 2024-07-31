1293ded5e20cac094be4b72a2b028b16795778f1
olli <oliverrausch2@web.de>
2024-07-31 21:31:15 +0200

    Move firebase secrets to separate file to exclude from git

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,7 +2,7 @@ import {initializeApp} from "firebase/app"
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import {getReactNativePersistence, initializeAuth} from "firebase/auth"
 import {getFirestore} from "firebase/firestore";
-import firebaseSecrets from "./firebaseSecrets";
+import firebaseSecrets from "./secrets/firebaseSecrets";
 
 const app = initializeApp(firebaseSecrets);
 
