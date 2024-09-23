351b18f27eda878edf550717af10687095ffdf2d
Volburt7 <example@mail.yes>
2024-09-23 19:13:35 +0200

    fix firebase import

--- app/_layout.js
+++ app/_layout.js
@@ -4,13 +4,12 @@ import {AuthProvider} from "../contexts/UserContext";
 import React from "react";
 import App from "../components/App";
 import {I18NProvider} from "../contexts/I18NContext";
+import "../firebaseConfig";
 
 /*
 * This is the entrypoint of the application and can be defined in some of the config files
 * */
 export default function HomeLayout() {
-    require("../firebaseConfig");
-
     return (
 
         <ThemeProvider>
