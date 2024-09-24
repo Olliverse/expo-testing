df896de6254d5872a03f510ccc615d94515b6be8
Volburt7 <example@mail.yes>
2024-09-24 11:02:02 +0200

    Jea jea, I know...

---
+++ secrets/firebaseSecrets.js
@@ -0,0 +1,10 @@
+const firebaseSecrets = {
+    apiKey: "AIzaSyDh6kOP1_kcslbos6TPNwIV9NNCQkjx4r4",
+    authDomain: "expo-testing-692aa.firebaseapp.com",
+    projectId: "expo-testing-692aa",
+    storageBucket: "expo-testing-692aa.appspot.com",
+    messagingSenderId: "595842757136",
+    appId: "1:595842757136:web:6466f30fbaf97ad1af5095",
+    measurementId: "G-75CT93218Q"
+}
+export default firebaseSecrets;
